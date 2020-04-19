39d127cec326c0e642d251351a5a585a35fd7e58
TruongDX8 <truonghusk17@gmail.com>
2020-04-19 15:46:55 +0700

    Final Test

---
+++ app/src/main/java/com/truongdx8/androidproject4/FinalTestActivity.java
@@ -0,0 +1,14 @@
+package com.truongdx8.androidproject4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_test);
+    }
+}
